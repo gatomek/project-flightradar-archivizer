492d5351502229cb9656b51b259eeea0c3c7b382
Gatomek <tomasz.galinski@gmail.com>
2026-08-04 20:50:50 +0200

    Radar Event Queue Listener added

---
+++ src/main/java/pl/gatomek/flightradar/eventsaver/EventSaverApplication.java
@@ -0,0 +1,13 @@
+package pl.gatomek.flightradar.eventsaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventSaverApplication.class, args);
+    }
+
+}
