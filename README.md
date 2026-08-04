06b75cc7ec22ada46eb499cff46c69a00757f06b
Gatomek <37158592+gatomek@users.noreply.github.com>
2026-08-04 22:31:13 +0200

    Radar Event Queue Listener added (#1)

---
+++ README.md
@@ -0,0 +1,4 @@
+# Radar Event Saver
+
+## Repo
+https://github.com/gatomek/project-flightradar-archivizer
---
+++ src/test/java/pl/gatomek/flightradar/eventsaver/saver/EventSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gatomek.flightradar.eventsaver.saver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSaverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
