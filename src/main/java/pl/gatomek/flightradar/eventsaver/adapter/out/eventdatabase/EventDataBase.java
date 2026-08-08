21f0093d64924e20a450043580aa5a6a342652e3
Gatomek <tomasz.galinski@gmail.com>
2026-08-08 11:51:44 +0200

    Saving events to db - review

--- src/main/java/pl/gatomek/flightradar/eventsaver/adapter/out/eventdatabase/EventDataBase.java
+++ src/main/java/pl/gatomek/flightradar/eventsaver/adapter/out/eventdatabase/EventDataBase.java
@@ -3,30 +3,25 @@ package pl.gatomek.flightradar.eventsaver.adapter.out.eventdatabase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import pl.gatomek.flightradar.eventsaver.adapter.out.eventdatabase.mapper.ToEntityMapper;
 import pl.gatomek.flightradar.eventsaver.adapter.out.eventdatabase.model.EventEntity;
+import pl.gatomek.flightradar.eventsaver.adapter.out.eventdatabase.repo.EventRepository;
 import pl.gatomek.flightradar.eventsaver.application.domain.model.Event;
 import pl.gatomek.flightradar.eventsaver.application.port.out.PersistEventPort;
-
-import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class EventDataBase implements PersistEventPort {
 
-    private final pl.gatomek.flightradar.eventsaver.adapter.out.eventdatabase.repo.EventRepository aircraftEventRepository;
+    private final EventRepository aircraftEventRepository;
 
     @Override
-    public void persistEvents(List<Event> events) {
-        List<EventEntity> entities = ToEntityMapper.INSTANCE.toEntities(events);
-        for( EventEntity e : entities) {
-            try {
-                aircraftEventRepository.save(e);
-            }
-            catch (Exception ex) {
-                log.error("persistEvent: {}", e.getIcao(), ex);
-            }
-        }
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void persistSingleEvent(Event event) {
+        EventEntity eventEntity = ToEntityMapper.INSTANCE.toEntity(event);
+        aircraftEventRepository.saveAndFlush(eventEntity);
     }
 }
